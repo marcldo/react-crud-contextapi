8aa24c6174fe18697b39df9688bda4adc2685eec
gitMarcldo <marcldo@users.noreply.github.com>
2022-11-03 18:01:15 -0400

    init routes and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./components/Home";
+import { AddUser } from "./components/AddUser";
+import { EditUser } from "./components/EditUser";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/add" component={AddUser} />
+          <Route path="/edit/:id" component={EditUser} />
+        </Switch>
+      </Router>
     </div>
   );
 }

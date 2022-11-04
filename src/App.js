e2629636c66aaa1b0c51a62890da0442a86495ad
gitMarcldo <marcldo@users.noreply.github.com>
2022-11-04 14:49:32 -0400

    global context added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Home } from "./components/Home";
 import { AddUser } from "./components/AddUser";
 import { EditUser } from "./components/EditUser";
-
+import { GlobalProvider } from "./context/GlobalState";
 import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -10,13 +10,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="app">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/add" component={AddUser} />
-          <Route path="/edit/:id" component={EditUser} />
-        </Switch>
-      </Router>
+      <GlobalProvider>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/add" component={AddUser} />
+            <Route path="/edit/:id" component={EditUser} />
+          </Switch>
+        </Router>
+      </GlobalProvider>
     </div>
   );
 }
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,29 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 import "./UserList.css";
 import { Link } from "react-router-dom";
 import { ListGroup, ListGroupItem, Button } from "reactstrap";
 
 export const UserList = () => {
+  const { users } = useContext(GlobalContext);
   return (
     <ListGroup className="mt-4">
-      <ListGroupItem className="li-item d-flex">
-        <strong>User One</strong>
-        <div className="ml-auto">
-          <Link className="btn btn-warning" to="/edit/1">
-            Edit
-          </Link>
-          <Button className="btn btn-danger ms-2">Delete</Button>
-        </div>
-      </ListGroupItem>
-      <ListGroupItem className="li-item d-flex">
-        <strong>User One</strong>
-        <div className="ml-auto">
-          <Link className="btn btn-warning" to="/edit/1">
-            Edit
-          </Link>
-          <Button className="btn btn-danger ms-2">Delete</Button>
-        </div>
-      </ListGroupItem>
+      {users.map((user) => (
+        <ListGroupItem className="li-item d-flex">
+          <strong>{user.name}</strong>
+          <div className="ml-auto">
+            <Link className="btn btn-warning" to={`/edit/${user.id}`}>
+              Edit
+            </Link>
+            <Button className="btn btn-danger ms-2">Delete</Button>
+          </div>
+        </ListGroupItem>
+      ))}
     </ListGroup>
   );
 };

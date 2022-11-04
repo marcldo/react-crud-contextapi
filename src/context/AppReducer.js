d2800b349f60e6ebb740c55e2affab13b77b375a
gitMarcldo <marcldo@users.noreply.github.com>
2022-11-04 19:07:32 -0400

    edit user functionality

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -10,6 +10,17 @@ export default (state, action) => {
       return {
         users: [action.payload, ...state.users],
       };
+    case "EDIT_USER":
+      const updateUser = action.payload;
+      const updateUsers = state.users.map((user) => {
+        if (user.id === updateUser.id) {
+          return updateUser;
+        }
+        return user;
+      });
+      return {
+        users: updateUsers,
+      };
     default:
       return state;
   }

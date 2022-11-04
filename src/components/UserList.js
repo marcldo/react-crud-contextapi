61a5d441ac69f2cc86b169febaa6a71fde01d12e
gitMarcldo <marcldo@users.noreply.github.com>
2022-11-04 19:14:23 -0400

    edit users complete

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -8,22 +8,28 @@ export const UserList = () => {
   const { users, removeUser } = useContext(GlobalContext);
   return (
     <ListGroup className="mt-4">
-      {users.map((user) => (
-        <ListGroupItem className="li-item d-flex">
-          <strong>{user.name}</strong>
-          <div className="ml-auto">
-            <Link className="btn btn-warning" to={`/edit/${user.id}`}>
-              Edit
-            </Link>
-            <Button
-              onClick={() => removeUser(user.id)}
-              className="btn btn-danger ms-2"
-            >
-              Delete
-            </Button>
-          </div>
-        </ListGroupItem>
-      ))}
+      {users.length > 0 ? (
+        <>
+          {users.map((user) => (
+            <ListGroupItem className="li-item d-flex" key={user.id}>
+              <strong>{user.name}</strong>
+              <div className="ml-auto">
+                <Link className="btn btn-warning" to={`/edit/${user.id}`}>
+                  Edit
+                </Link>
+                <Button
+                  onClick={() => removeUser(user.id)}
+                  className="btn btn-danger ms-2"
+                >
+                  Delete
+                </Button>
+              </div>
+            </ListGroupItem>
+          ))}
+        </>
+      ) : (
+        <h4 className="text-center">No User</h4>
+      )}
     </ListGroup>
   );
 };
